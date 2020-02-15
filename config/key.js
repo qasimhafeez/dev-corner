98eb5df326ed575714cf80e9c9bd5a1397e36c37
Qasim Hafeez <qasimh556@gmail.com>
2020-02-14 23:37:00 -0800

    initial commit

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:
+      "mongodb+srv://admin:admin@nodecluster-oqgkx.mongodb.net/test?retryWrites=true&w=majority"
+  };
+  
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:admin@nodecluster-oqgkx.mongodb.net/test?retryWrites=true&w=majority"
+};
