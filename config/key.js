49ff48b1182858ed5146057036004714aca43f64
Qasim Hafeez <qasimh556@gmail.com>
2020-02-15 01:54:10 -0800

    modiefied

--- config/key.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI:
-      "mongodb+srv://admin:admin@nodecluster-oqgkx.mongodb.net/test?retryWrites=true&w=majority"
-  };
-  
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+// Load user model
+const User = require("../../models/User");
+
 // @route   GET api/users/test
 // @desc    Test post route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Users Work" }));
+
+// @route   GET api/users/register
+// @desc    Register a user
+// @access  Public
+router.post("/register", (req, res) => {
+    
+});
 
 module.exports = router;
