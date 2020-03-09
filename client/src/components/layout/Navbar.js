7657d9098ee6b3b5aac9de3dc2d05afc8a574277
Qasim Hafeez <qasimh556@gmail.com>
2020-03-09 11:00:25 +0400

    changed navigation

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -19,6 +19,17 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link to="/feed" className="nav-link">
+            Post Feed
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/dashboard" className="nav-link">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
+          {/* eslint-disable-next-line */}
           <a
             href="#"
             className="nav-link"
